aafc40581691ef72eb7a721919fd84429524d707
Javenkm <Javenkm@gmail.com>
2021-06-22 21:09:49 -0700

    init commit

---
+++ Manning_Javen_Array_AlgoNotes.js
@@ -0,0 +1,65 @@
+// var sum = 0
+// var testArr = [6, 3, 5, 1, 2, 4]
+// for(var num = 0; num < testArr.length; num = num + 1) {
+//     sum += testArr[num];
+//     console.log("Num: " + testArr[num], ("Sum: ") + sum);
+// }
+
+
+
+// [0, 3, 10, 3, 8, 20]
+
+
+// var sum = 0;
+// var testArr = [6, 3, 5, 1, 2, 4];
+// for(var i = 0; i < testArr.length; i++) {
+//     sum = (testArr[i] * testArr[i]);
+//     console.log(sum);
+// }
+
+
+// function highFive(num){
+//     for(var i=0; i<num; i++){
+//         if(i == 5){
+//             console.log("High Five!");
+//         }
+//         else{
+//             console.log(i);
+//         }
+//     }
+// }
+
+// function arr() {
+//     for(var i = 1; i <= 255; i++) {
+//         console.log(i);
+//     }
+//     return arr;
+// }
+// console.log(arr());
+
+// function arr1() {
+
+//     var arr1 = [];
+
+//     for (var i = 1; i < 256; i++) {
+//         arr1.push(i);
+//     }
+
+//     return arr1;
+
+// }
+
+// console.log(arr1());
+
+
+// function evenNums() {
+//     var sum = 0;
+//     for(var i = 0; i <= 1000; i++) {
+//         if(i % 2 == 0) {
+//             sum = sum + i
+//         }
+//     }
+//     return sum;
+// }
+// console.log(evenNums());
+
---
+++ Manning_Javen_Intermediate_Foundation_I.js
@@ -0,0 +1,2 @@
+// 1.) Sigma - Implement function sigma(num) that, given a number, returns the sum of all positive integers up to the given number (inclusive). Example: sigma(3) = 6 (or 1+2+3); sigma)5_ = 15(or 1+2+3+4+5).
+
